75a787da60f09b35ea2895fbb9c596047a81d0f5
{zsmalla} <{malla7454@gmail.com}>
2022-11-15 15:17:27 +0900

    initial commit

--- src/chapter1/1_탐색과정렬(1)/1920_python_임지수.py
+++ src/chapter1/1_탐색과정렬(1)/1920_python_임지수.py
@@ -1,1 +1,24 @@
-# initial commit
+import sys
+
+input = sys.stdin.readline
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return True
+        elif arr[mid] > target:
+            high = mid-1
+        else:
+            low = mid+1
+    return False
+
+N = int(input())
+arr = sorted(list(map(int, input().split())))
+M = int(input())
+targets = map(int, input().split())
+
+for target in targets:
+    print(1) if binary_search(arr, target) else print(0)
