892cd8fafe83e9149b1210b53973b12608895bb9
황보승화 <seunghwa5156@gmail.com>
2022-12-17 08:57:17 +0900

    [황보승화] 1-1 문제 풀이(2750)

--- src/chapter1/1_탐색과정렬(1)/황보승화/2750_python_황보승화.py
+++ src/chapter1/1_탐색과정렬(1)/황보승화/2750_python_황보승화.py
@@ -1,1 +1,43 @@
-#test
+# 이거 아래 코드로 했을 때는 정답인데 위 코드로 했을 때는 틀렸다는데 이유를 모르겠어...
+def counting_sort(arr):
+    count = [0] * (max(arr) + 1)
+
+    for num in arr:
+        count[num] += 1
+
+    for i in range(1, len(count)):
+        count[i] += count[i - 1]
+
+    result = [0] * (len(arr))
+
+    for num in arr:
+        idx = count[num]
+        result[idx - 1] = num
+        count[num] -= 1
+
+    for l in result:
+        print(l)
+
+
+N = int(input())
+arr = []
+
+for i in range(N):
+    arr.append(int(input()))
+
+counting_sort(arr)
+
+# ---------------------------
+# N = int(input())
+# arr = []
+
+# for i in range(N):
+#   arr.append(int(input()))
+
+# for j in range(len(arr)):
+#   for k in range(len(arr)):
+#     if arr[j] < arr[k]:
+#         arr[j], arr[k] = arr[k], arr[j]
+
+# for l in arr:
+#   print(l)
