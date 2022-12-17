79c53ca17f409ea7182763c3e7fd29d821c2f86c
tnddk <96976690+tnddk@users.noreply.github.com>
2022-12-17 11:12:55 +0900

    2751 (#32)
    * [황보승화] 1-1 문제 풀이(2750)
    
    * [황보승화] 1-1 문제 풀이(2750)
    
    * [황보승화] 1-1 문제 풀이(2751)
    
    * Update 2750_python_황보승화.py
    
    merge conflict 해결
    
    Co-authored-by: JISU LIM <72483874+zsmalla@users.noreply.github.com>

--- src/chapter1/1_탐색과정렬(1)/황보승화/2750_python_황보승화.py
+++ src/chapter1/1_탐색과정렬(1)/황보승화/2750_python_황보승화.py
@@ -1,32 +1,66 @@
-# 이거 아래 코드로 했을 때는 정답인데 위 코드로 했을 때는 틀렸다는데 이유를 모르겠어...
-def counting_sort(arr):
-    count = [0] * (max(arr) + 1)
+#1번
+# heapq 라이브러리 사용
+import heapq
 
-    for num in arr:
-        count[num] += 1
+def heapsort(iterable):
+    h = []
+    result = []
 
-    for i in range(1, len(count)):
-        count[i] += count[i - 1]
+    # 모든 원소를 차례대로 힙에 삽입
+    for value in iterable:
+        heapq.heappush(h, value)  # heapq.heappush() : 힙에서 원소를 삽입할 때
 
-    result = [0] * (len(arr))
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h)):
+        result.append(heapq.heappop(h))  # heapq.heappop() : 합에서 원소를 꺼낼 때 항상 가장 작은 원소를 꺼낸다.
 
-    for num in arr:
-        idx = count[num]
-        result[idx - 1] = num
-        count[num] -= 1
-
-    for l in result:
-        print(l)
-
+    return result
 
 N = int(input())
-arr = []
+unsorted_list = []
 
 for i in range(N):
-    arr.append(int(input()))
+    unsorted_list.append(int(input()))
 
-counting_sort(arr)
+result = heapsort(unsorted_list)
+for j in result:
+    print(j)
 
+#----------------------------------------------------
+
+#2번
+# def counting_sort(arr):
+#     count = [0] * (max(arr) + 1)
+#
+#     for num in arr:
+#         count[num] += 1
+#
+#     for i in range(1, len(count)):
+#         count[i] += count[i - 1]
+#
+#     result = [0] * (len(arr))
+#
+#     for num in arr:
+#         idx = count[num]
+#         result[idx - 1] = num
+#         count[num] -= 1
+#
+#     for l in result:
+#         print(l)
+#
+#
+# N = int(input())
+# arr = []
+#
+# for i in range(N):
+#     arr.append(int(input()))
+#
+# counting_sort(arr)
+
+# ---------------------------
+#3번
+# 이거 아래 코드로 했을 때는 정답인데 위 코드로 했을 때는 틀렸다는데 이유를 모르겠어...
+# ---------------------------
 # ---------------------------
 # N = int(input())
 # arr = []
@@ -41,3 +75,4 @@ counting_sort(arr)
 
 # for l in arr:
 #   print(l)
+# ---------------------------
