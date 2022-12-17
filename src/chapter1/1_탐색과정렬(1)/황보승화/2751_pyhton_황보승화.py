70f93f3c478a3a084e44c857f97931451f579b12
tnddk <96976690+tnddk@users.noreply.github.com>
2022-12-17 12:49:24 +0900

    10989 (#34)
    * [황보승화] 1-1 문제 풀이(2750)
    
    * [황보승화] 1-1 문제 풀이(2750)
    
    * [황보승화] 1-1 문제 풀이(2751)
    
    * [황보승화] 1-1 문제 풀이(2751 수정, 10989 test)
    
    * [황보승화] 1-1 문제 풀이(2751 수정, 10989 test)
    
    Co-authored-by: JISU LIM <72483874+zsmalla@users.noreply.github.com>

--- src/chapter1/1_탐색과정렬(1)/황보승화/2751_pyhton_황보승화.py
+++ src/chapter1/1_탐색과정렬(1)/황보승화/2751_pyhton_황보승화.py
@@ -1,12 +1,29 @@
-# heapq 라이브러리 사용
+# 가장 빠른 방법! - 백준 통과(O)
+import sys
+
 import heapq
 
 N = int(input())
 result = []
 
 for i in range(N):
+
+    result.append(int(sys.stdin.readline().rstrip()))
+
+result.sort()
+for i in result:
+    print(i)
+#--------------------------------------------------
+# heapq 라이브러리 사용 - 백준 통과(X)
+# import heapq
+#
+# N = int(input())
+# result = []
+#
+# for i in range(N):
+#     heapq.heappush(result,input())
+# for l in range(N):
+#     print(heapq.heappop(result))
     heapq.heappush(result,input())
 for l in range(N):
     print(heapq.heappop(result))
-
-
