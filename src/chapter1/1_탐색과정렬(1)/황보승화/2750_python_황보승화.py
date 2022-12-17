a9d9801532b70d4651ab23f3daf089ac5a852ec1
tnddk <96976690+tnddk@users.noreply.github.com>
2022-12-17 12:44:51 +0900

    2750new (#33)
    * [황보승화] 1-1 문제 풀이(2750)
    
    * [황보승화] 1-1 문제 풀이(2750)
    
    * [황보승화] 1-1 문제 풀이(2751)
    
    * [황보승화] 1-1 문제 풀이(2750_수정, 카운팅정렬 수정)
    
    Co-authored-by: JISU LIM <72483874+zsmalla@users.noreply.github.com>

--- src/chapter1/1_탐색과정렬(1)/황보승화/2750_python_황보승화.py
+++ src/chapter1/1_탐색과정렬(1)/황보승화/2750_python_황보승화.py
@@ -27,41 +27,9 @@ for j in result:
     print(j)
 
 #----------------------------------------------------
+#
+# #2번
 
-#2번
-# def counting_sort(arr):
-#     count = [0] * (max(arr) + 1)
-#
-#     for num in arr:
-#         count[num] += 1
-#
-#     for i in range(1, len(count)):
-#         count[i] += count[i - 1]
-#
-#     result = [0] * (len(arr))
-#
-#     for num in arr:
-#         idx = count[num]
-#         result[idx - 1] = num
-#         count[num] -= 1
-#
-#     for l in result:
-#         print(l)
-#
-#
-# N = int(input())
-# arr = []
-#
-# for i in range(N):
-#     arr.append(int(input()))
-#
-# counting_sort(arr)
-
-# ---------------------------
-#3번
-# 이거 아래 코드로 했을 때는 정답인데 위 코드로 했을 때는 틀렸다는데 이유를 모르겠어...
-# ---------------------------
-# ---------------------------
 # N = int(input())
 # arr = []
 
@@ -75,4 +43,3 @@ for j in result:
 
 # for l in arr:
 #   print(l)
-# ---------------------------
