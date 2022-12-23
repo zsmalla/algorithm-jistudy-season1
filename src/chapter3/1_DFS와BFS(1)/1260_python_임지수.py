38843fcd2602e8abcf3951b7bb1e002bb9a32e2c
JISU LIM <72483874+zsmalla@users.noreply.github.com>
2022-12-23 15:09:22 +0900

    [임지수] 3-1 개념 정리 & 문제 풀이(1260) (#51)

--- src/chapter3/1_DFS와BFS(1)/1260_python_임지수.py
+++ src/chapter3/1_DFS와BFS(1)/1260_python_임지수.py
@@ -1,1 +1,40 @@
-# 3-1 start
+import sys
+from collections import deque, defaultdict
+import heapq
+
+input = sys.stdin.readline
+
+N, M, V = map(int, input().rstrip().split())
+graph = defaultdict(list)
+
+for _ in range(M):
+    node1, node2 = map(int, input().rstrip().split())
+    graph[node1].append(node2)
+    graph[node2].append(node1)
+
+for i in range(1, N+1):
+    graph[i].sort()
+
+def dfs(node:int, visited:list):
+    visited.append(node)
+
+    for next_node in graph[node]:
+        if next_node not in visited:
+            dfs(next_node, visited)
+
+    return visited
+def bfs(node:int, visited:list):
+    queue = deque([node])
+    visited.append(node)
+
+    while queue:
+        current_node = queue.popleft()
+        for next_node in graph[current_node]:
+            if next_node not in visited:
+                queue.append(next_node)
+                visited.append(next_node)
+
+    return visited
+
+print(*dfs(V, []))
+print(*bfs(V, []))
