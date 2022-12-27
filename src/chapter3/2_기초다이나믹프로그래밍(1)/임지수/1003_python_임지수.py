23d69cf536f3a19b09b4dfce820665a39518c4ce
JISU LIM <72483874+zsmalla@users.noreply.github.com>
2022-12-27 15:13:09 +0900

    [임지수] 3-2 문제 풀이(1003) (#61)

--- src/chapter3/2_기초다이나믹프로그래밍(1)/임지수/1003_python_임지수.py
+++ src/chapter3/2_기초다이나믹프로그래밍(1)/임지수/1003_python_임지수.py
@@ -1,1 +1,12 @@
-# 3-2 start!
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+dp = [0, 1]+[-1 for _ in range(2, 41)]
+zero_one = [(1, 0), (0, 1)]
+for i in range(2, 42):
+    zero_one.append((zero_one[i-1][0]+zero_one[i-2][0], zero_one[i-1][1]+zero_one[i-2][1]))
+for _ in range(T):
+    n = int(input())
+    print(*zero_one[n])
