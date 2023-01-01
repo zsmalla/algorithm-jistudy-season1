c4d93d37528899d2e3b2b0513017440f69137b0d
{zsmalla} <malla7454@gmail.com>
2023-01-01 23:22:23 +0900

    [임지수] 3-4 문제 풀이(11726)

--- src/chapter3/4_기초다이나믹프로그래밍(2)/11726_python_임지수.py
+++ src/chapter3/4_기초다이나믹프로그래밍(2)/11726_python_임지수.py
@@ -1,1 +1,18 @@
-# 3-4 start!
+import sys
+
+input = sys.stdin.readline
+
+def dp(n):
+    if memo[n] != 0:
+        return memo[n]
+    else:
+        memo[n] = dp(n-1) + dp(n-2)
+        return memo[n]
+
+n = int(input())
+if n < 3:
+    print(n)
+    exit(0)
+memo = [0 for _ in range(n+1)]
+memo[1], memo[2] = 1, 2
+print(dp(n)%10007)
